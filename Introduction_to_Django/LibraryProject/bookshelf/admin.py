f7b07f5863193ac29d5e22cee8b7449cdf39a38b
Sherif Mohamed <sherif425@gmail.com>
2025-08-31 10:43:41 +0300

    adding admin, list dispalys, search field, list display

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import Book
 
+
+
+class BookAdmin(admin.ModelAdmin):
+    # Display these fields in the list view
+    list_display = ('title', 'author', 'publication_year')
+
+    # Enable search by title and author
+    search_fields = ('title', 'author')
+
+    # Add filter for publication year
+    list_filter = ('publication_year',)
+    
+    
 # Register your models here.
 admin.site.register(Book)
