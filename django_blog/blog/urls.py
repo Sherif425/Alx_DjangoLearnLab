9d4e0e3adac1d0b37afe8f609042b978c121f9cf
paygateitteam <73990524+paygateitteam@users.noreply.github.com>
2025-10-04 15:12:46 +0300

    Task 1 - adding path('profile/<str:username>/'

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -16,4 +16,6 @@ urlpatterns = [
     path("login/", views_auth.login_view, name="login"),
     path("logout/", views_auth.logout_view, name="logout"),
     path("register/", views_auth.register, name="register"),
+
+    path("profile/<str:username>/", views.ProfileView.as_view(), name="profile"),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required, UserPassesTestMixin
 from .models import Post
-
+from django.contrib.auth.models import User
 
 # List all posts
 class PostListView(ListView):
@@ -47,3 +47,18 @@ class PostDeleteView(LoginRequiredMixin, DeleteView):
     template_name = "blog/post_confirm_delete.html"
     context_object_name = "post"
     success_url = reverse_lazy("blog:post-list")
+
+
+class ProfileView(ListView):
+    model = Post
+    template_name = "blog/profile.html"
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        user = User.objects.get(username=self.kwargs.get("username"))
+        return Post.objects.filter(author=user).order_by("-id")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["profile_user"] = User.objects.get(username=self.kwargs.get("username"))
+        return context
