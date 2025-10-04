b778ca1460f925db146b19bdd6fdbe333de3d8b1
paygateitteam <73990524+paygateitteam@users.noreply.github.com>
2025-10-04 11:01:06 +0300

    Task0: 0. Initial Setup and Project Configuration for a Django Blog- adding postgresql config

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -79,10 +79,22 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django-blog',       # database name
+        'USER': 'dbadmin',       # database username
+        'PASSWORD': 'p@SS0987vcx', # database password
+        'HOST': 'localhost',          # or the IP address of your DB server
+        'PORT': '5432',               # default PostgreSQL port
     }
 }
 
